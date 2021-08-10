961d10dbda53d2b4c2a41f6181f774d85f592609
Eva Jutzeler <eju@hoststar.ch>
2021-08-10 09:55:14 +0200

    #204 Travelon: The Feature element edit square is overlapping the menu icon.

--- scripts.js
+++ scripts.js
@@ -8,13 +8,17 @@ $(document).ready(function() {
 	// Make language selector invisible on scrolling down
 	// ------------------------------
 
-	$('#main').on('scroll', function () {
-		var scrollTop = $('#main').scrollTop();
+	$(window).on('scroll', function () {
+		var scrollTop = $(window).scrollTop();
+
+		console.log(scrollTop);
 
 		if (scrollTop > 50) {
 			$('.language-selector').addClass('language-selector--hide');
+			$('a.pull').addClass('nav-scrolled');
 		} else {
 			$('.language-selector').removeClass('language-selector--hide');
+			$('a.pull').removeClass('nav-scrolled');
 		}
 	});
 });
