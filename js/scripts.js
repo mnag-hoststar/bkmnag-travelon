e751bb4aa2b47919c92c82943da04e627991747c
Eva Jutzeler <eju@hoststar.ch>
2022-05-16 10:54:38 +0200

    #460 Travelon: Fix template bugs for migration

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,7 @@
 
 $(document).ready(function() {
 	
-	// ------------------------------
 	// Make language selector invisible on scrolling down
-	// ------------------------------
 
 	$(window).on('scroll', function () {
 		var scrollTop = $(window).scrollTop();
@@ -20,5 +18,11 @@ $(document).ready(function() {
 			$('.language-selector').removeClass('language-selector--hide');
 			$('a.pull').removeClass('nav-scrolled');
 		}
+	});
+
+	// Hides other submenus when a main menu point with a submenu is clicked.
+
+	$(".extendednavigation > ul > li").click(function() {
+		$(this).siblings(".extendednavigation > ul > li.folder").removeClass("folder-open");
 	});
 });
